d7742f83854d35abd3bcbdb62bb7f40e846bd5d4
Matt Wiens <mwiens91@gmail.com>
2018-10-23 18:19:30 -0700

    Add function to get all of a project's tickets

--- jira_ticket_migrate/jira.py
+++ jira_ticket_migrate/jira.py
@@ -1,16 +1,7 @@
 """Contains Jira related functionality."""
 
+from typing import List
 from jira import JIRA as Jira
-
-
-class JiraProject:
-    """Representation of a Jira project.
-
-    Attributes:
-    """
-
-    def __init__(self):
-        pass
 
 
 class JiraTicket:
@@ -88,3 +79,81 @@ def create_blank_ticket(project: str) -> JiraTicket:
         summary="Blank ticket",
         type_="Task",
     )
+
+
+def get_project_tickets(
+    jira: Jira, project: str, insert_blank_tickets: bool = True
+) -> List[JiraTicket]:
+    """Get all tickets from a project in ascending order.
+
+    This will fill in missing tickets with blank tickets if
+    insert_blank_tickets is set to True.
+
+    Args:
+        project: The project name.
+        insert_blank_tickets (optional): If an intermediate ticket is
+            not defined, fill in a blank ticket in its place. For
+            example, if the project name is PROJ and PROJ-1 and PROJ-3
+            exist on the Jira but not PROJ-2, this will fill a blank
+            ticket for PROJ-2.  Defaults to True.
+
+    Returns:
+        A list of JiraTickets from ticket number 1 to ticket N, where N
+        is the last ticket number.
+    """
+    tickets = []
+
+    # Keep track of what a ticket "should" be if we're inserting blank
+    # tickets
+    ticket_counter = 1
+
+    # Offsets for the API
+    init = 0
+    size = 100
+
+    # Fetch from the API until there's no tickets left
+    while True:
+        start = init * size
+
+        tickets = jira.search_issues("project=%s" % project, start, size)
+
+        # Check if we've reached the end
+        if not tickets:
+            break
+
+        # Add in the tickets
+        for ticket in tickets:
+            this_ticket_num = int(ticket.key.split("-")[-1])
+
+            # Insert blank tickets as necessary
+            while insert_blank_tickets and ticket_counter < this_ticket_num:
+                tickets.append(create_blank_ticket(project))
+
+                ticket_counter += 1
+
+            # Insert *this* ticket. First deal with attributes that we
+            # have to be careful with Nones with. Then make the ticket.
+            description = ticket.fields.description
+
+            if description is None:
+                description = ""
+
+            resolution = ticket.fields.resolution
+
+            if resolution is not None:
+                resolution = resolution.name
+
+            tickets.append(
+                JiraTicket(
+                    description=description,
+                    priority=ticket.fields.priority.name,
+                    project=project,
+                    resolution=resolution,
+                    source_link=ticket.permalink(),
+                    status=ticket.fields.status.name,
+                    summary=ticket.fields.summary,
+                    type_=ticket.fields.issuetype.name,
+                )
+            )
+
+    return tickets
